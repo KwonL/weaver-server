8727e5132c8d1564a348cfc8653b307640bcda92
KwonL <lkh116@snu.ac.kr>
2020-08-28 02:28:51 +0900

    Add README.md | Implement basic test

---
+++ src/test/java/com/miracle/weaver/user/UserControllerTest.java
@@ -0,0 +1,31 @@
+package com.miracle.weaver.user;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void testUserListController() throws Exception {
+        mockMvc.perform(get("/api/user").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+}
